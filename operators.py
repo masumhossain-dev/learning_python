862681ba8c4d2e50699cc485575fff7d000e3089
masumhossain-dev <masumhossain.dev@gmail.com>
2025-05-19 00:54:56 +0600

    practicing operators and operators built in methods

---
+++ operators.py
@@ -0,0 +1,42 @@
+import math
+a = 21
+b = 5
+
+#sum
+sum = a + b
+print(sum)
+
+#substruction
+sub = a - b
+print(sub)
+
+#multiplication
+mul = a * b
+print(mul)
+
+#dividation
+div = a/b
+div2 = a//b #for avoid factiobnal result. it give an intiger value.
+print(div, div2)
+
+#order or power
+power = a^b
+print(power)
+
+#reminder
+rem = a%b
+print(rem)
+
+
+
+#operators methods(these methods works for only float type values)
+
+#ceil
+num = 3.1416
+print(math.ceil(num))
+
+#floor
+print(math.floor(num))
+
+#round
+print(round(num))
