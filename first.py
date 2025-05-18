f1fc64357f57adefbea9d4cf5df2392fdc99f719
masumhossain-dev <masumhossain.dev@gmail.com>
2025-05-19 00:35:16 +0600

    string methods

--- first.py
+++ first.py
@@ -12,3 +12,12 @@ print(uni[len(uni)-1])
 
 #print last index on a string(short method)
 print(uni[-1]) #it also called minus(-) indexing. if i pass -2 as a index number, it will print second last number of the string.
+
+#string methods
+name = 'masum hossian'
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.center(20))
+print(name.count('h')) #and so on...
