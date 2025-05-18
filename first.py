52bc552296ba87908cfa135ad2a2b4bc63e76193
masumhossain-dev <masumhossain.dev@gmail.com>
2025-05-18 23:55:08 +0600

    print specific index numbers

--- first.py
+++ first.py
@@ -2,7 +2,13 @@ a = 10
 print(type(a))
 
 uni = 'Bangladesh University of Business and Technology'
-
 print(len(uni))
-print(uni[-1])
-#ksndnsan
+
+
+#String indexing
+
+#print last index on a string(long method)
+print(uni[len(uni)-1])
+
+#print last index on a string(short method)
+print(uni[-1]) #it also called minus(-) indexing. if i pass -2 as a index number, it will print second last number of the string.
