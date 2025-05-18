d3e65b1b1806b92100d1a7c0a52c3509c077f9c2
masumhossain-dev <masumhossain.dev@gmail.com>
2025-05-19 01:27:10 +0600

    learn multiple procedure of string formating

--- string _formating.py
+++ string _formating.py
@@ -1,8 +1,11 @@
 userName = input('What is your name?')
 userAge = input('and How much your age?')
-txt1 = 'Hello Mr. {}, \b Your age is {}.'.format(userName, userAge)
+txt1 = 'Hello Mr. {}, \n Your age is {}.'.format(userName, userAge)
 #another procedure
-txt2 = 'Hello Mr. {userName}, \b Your age is {userAge}.'.format(userName = userName, userAge = userAge)
+txt2 = 'Hello Mr. {userName}, \n Your age is {userAge}.'.format(userName = userName, userAge = userAge)
+#another procedure
+txt3 = f'Hello Mr. {userName},\n Your age is {userAge}.'
 
 print(txt1)
 print(txt2)
+print(txt3)
